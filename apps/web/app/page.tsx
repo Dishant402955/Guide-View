153e05980d6f1af5bad5adbcd20dec361446437c
Dishant radadiya <dishantradadiya3036@gmail.com>
2026-02-26 19:39:45 +0530

    setup shadcn & theming & dark-light mode

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,15 @@
+import { ThemeTogglerButton } from "@/components/animate-ui/components/buttons/theme-toggler";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div></div>
-  );
+	return (
+		<div className="h-full w-full flex justify-center items-center p-2">
+			<Button>Hello</Button>
+			<ThemeTogglerButton
+				modes={["dark", "light"]}
+				size={"lg"}
+				direction="ltr"
+			/>
+		</div>
+	);
 }
