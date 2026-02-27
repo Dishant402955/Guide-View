646643c2bf7a089f819414cf6549035945287b00
MeetParmar2302 <meet.parmar.2302@gmail.com>
2026-02-27 14:30:40 +0530

    Setup Drizzle with PostgreSQL

---
+++ apps/web/db/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+// import * as schema from "../drizzle/schema";
+// import * as relations from "../drizzle/relations";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+// export const db = drizzle(pool, { schema });
